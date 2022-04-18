4ce069efc95401bb705007eb5941bacd8f40ddfe
Luiz Felipe <felipefelizatti215@gmail.com>
2022-04-18 03:03:26 -0300

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,19 +3,49 @@ import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import styles from './styles.module.scss';
 
 const Footer = () => (
-  <div className={styles.footer}>
-    <div className={styles.social_medias}>
-      <a href="#">
-        <FaFacebookF />
-      </a>
-      <a href="#">
-        <FaInstagram />
-      </a>
-      <a href="#">
-        <FaTwitter />
-      </a>
+  <footer className={styles.footer}>
+    <div>
+      <div>
+        <h1>Sobre nós</h1>
+        <p>Nossa empresa</p>
+        <p>Nosso café</p>
+        <p>Atendimento ao cliente</p>
+      </div>
+
+      <div>
+        <h1>Impacto Social</h1>
+        <p>Comunidade</p>
+        <p>Meio Ambiente</p>
+        <p>Histórias Starbucks</p>
+      </div>
+
+      <div>
+        <h1>Starbucks Rewards</h1>
+        <p>Baixe o Aplicativo</p>
+        <p>Termos e condições do Starbucks Card</p>
+        <p>Termos e condições do Starbucks Promoções</p>
+      </div>
     </div>
-  </div>
+
+    <hr />
+    <div>
+      <div className={styles.social_medias}>
+        <a href="#">
+          <FaFacebookF />
+        </a>
+        <a href="#">
+          <FaInstagram />
+        </a>
+        <a href="#">
+          <FaTwitter />
+        </a>
+      </div>
+
+      <p>Política de privacidade | Política de troca de produto | Termos de uso | Política de Cookies</p>
+
+      <p>2022 Starbucks Coffee Company. Todos os direitos reservados.</p>
+    </div>
+  </footer>
 );
 
 export { Footer };
