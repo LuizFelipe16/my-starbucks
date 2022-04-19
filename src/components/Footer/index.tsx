d210fae8852c367934a2ade10df5a9ad4597da88
Luiz Felipe <felipefelizatti215@gmail.com>
2022-04-19 14:26:37 -0300

    README

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -48,7 +48,7 @@ const Footer = () => (
 
       <p>Política de privacidade | Política de troca de produto | Termos de uso | Política de Cookies</p>
 
-      <p>2022 Luiz Felipe. Todos os direitos reservados.</p>
+      <p>2022 by Luiz Felipe.</p>
     </div>
   </footer>
 );
