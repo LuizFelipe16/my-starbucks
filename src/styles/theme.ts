a4e1e5dd3239e67c5b8f0c10711ebeec67094a35
Luiz Felipe <felipefelizatti215@gmail.com>
2022-04-19 01:10:49 -0300

    add chakra

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    bg: {
+      100: '#ddd',
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins"
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'bg.100',
+      }
+    }
+  }
+});
